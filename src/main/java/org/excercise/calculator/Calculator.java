37d23a12105f35c2b7bb8ad9fecdd7c9872a51c2
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-28 14:18:56 +0200

    Commit

--- src/main/java/org/excercise/calculator/Calculator.java
+++ src/main/java/org/excercise/calculator/Calculator.java
@@ -1,2 +1,26 @@
-package org.excercise.calculator;public class Calculator {
+package org.excercise.calculator;
+
+public class Calculator {
+    public static float add(float num1, float num2) {
+        return num1 + num2;
+    }
+
+    public static float subtract(float num1, float num2) {
+        return num1 - num2;
+    }
+
+    public static float divide(float num1, float num2) {
+        if (num2 == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return num1 / num2;
+    }
+
+    public static float multiply(float num1, float num2) {
+        return num1 * num2;
+    }
+
+    private Calculator() {
+        throw new UnsupportedOperationException("Utility class");
+    }
 }
--- src/test/java/org/excercise/calculator/CalculatorTest.java
+++ src/test/java/org/excercise/calculator/CalculatorTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package org.excercise.calculator;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class CalculatorTest {
-  
+
+    @Test
+    public void testAdd() {
+        assertEquals(5.0f, Calculator.add(2.0f, 3.0f), 0.001f);
+        assertEquals(0.0f, Calculator.add(-1.0f, 1.0f), 0.001f);
+        assertEquals(-5.0f, Calculator.add(-2.0f, -3.0f), 0.001f);
+    }
+
+    @Test
+    public void testSubtract() {
+        assertEquals(-1.0f, Calculator.subtract(2.0f, 3.0f), 0.001f);
+        assertEquals(-2.0f, Calculator.subtract(-1.0f, 1.0f), 0.001f);
+        assertEquals(1.0f, Calculator.subtract(-2.0f, -3.0f), 0.001f);
+    }
+
+    @Test
+    public void testMultiply() {
+        assertEquals(6.0f, Calculator.multiply(2.0f, 3.0f), 0.001f);
+        assertEquals(-1.0f, Calculator.multiply(-1.0f, 1.0f), 0.001f);
+        assertEquals(6.0f, Calculator.multiply(-2.0f, -3.0f), 0.001f);
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(2.0f, Calculator.divide(6.0f, 3.0f), 0.001f);
+        assertEquals(-1.0f, Calculator.divide(-3.0f, 3.0f), 0.001f);
+        assertEquals(1.5f, Calculator.divide(-3.0f, -2.0f), 0.001f);
+    }
+
+    @Test
+    public void testDivideByZero() {
+        assertThrows(ArithmeticException.class, () -> Calculator.divide(1.0f, 0.0f));
+    }
 }
